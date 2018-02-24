68601592b5844d5da217f50bc471cde94da5cde9
Unknown <marcis.vijups@gmail.com>
2018-02-24 22:22:31 +0200

    Solution for 28 reklamas

--- 1_star/28_reklamas/28_reklamas/main.c
+++ 1_star/28_reklamas/28_reklamas/main.c
@@ -3,12 +3,26 @@
 
 int main()
 {
-    FILE *inFile = fopen("reklamas.dat","r+");
+	unsigned long long int n, k, m, lapas_sodien, lapas_kopa;
 
+    FILE *inFile = fopen("reklamas.dat","r+");
+	fscanf(inFile, "%llu %llu %llu",&n, &k, &m);
     fclose(inFile);
 
+    int dienas = 1;
+    lapas_kopa = n;
+
+
+    while(lapas_kopa < m)
+	{
+		dienas++;
+		lapas_sodien = n + k;
+		n = lapas_sodien;
+		lapas_kopa = lapas_kopa + lapas_sodien;
+
+	}
 	FILE *outFile = fopen("reklamas.rez","w+");
-	fprintf(outFile, "lul\n");
+	fprintf(outFile, "%d\n", dienas);
 	fclose(outFile);
     return 0;
 }
