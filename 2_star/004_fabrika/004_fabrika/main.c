ffe7c3b0e45ea76058e13df96a3b91cfaacf2054
Mārcis Vijups <marcis.vijups@gmail.com>
2023-12-29 16:42:57 +0000

    Also added more inteligent math based solution using GCD and LCM

--- 2_star/004_fabrika/004_fabrika/main.c
+++ 2_star/004_fabrika/004_fabrika/main.c
@@ -4,6 +4,21 @@
 #include <stdlib.h>
 #include <string.h>
 
+
+// gcd - greatest common divisor euclidean algorithm
+uint64_t gcd(uint64_t a, uint64_t b)
+{
+	uint64_t tmp;
+	while (b != 0)
+	{
+		tmp = b;
+		b	= a % b;
+		a	= tmp;
+	}
+	return a;
+}
+
+
 int32_t main()
 {
 	FILE* infile  = fopen("fabrika.in", "r");
@@ -16,46 +31,60 @@ int32_t main()
 	m++;
 	uint64_t nokrasoti, nolakoti, neapstradati, apstradati;
 	nokrasoti = nolakoti = neapstradati = apstradati = 0;
+	// inteligent mathematically approach
+	uint64_t nokrasoti1, nolakoti1, neapstradati1, apstradati1;
+	nokrasoti1 = nolakoti1 = neapstradati1 = apstradati1 = 0;
+	nokrasoti1											 = k / (n);
+	nolakoti1											 = k / (m);
+	// LCM - least common multiple
+	uint64_t lcm  = (n) * (m) / gcd(n, m);
+	neapstradati1 = k / (lcm);
+	nokrasoti1 -= neapstradati1;
+	nolakoti1 -= neapstradati1;
+	apstradati1 = k - nokrasoti1 - nolakoti1 - neapstradati1;
+	fprintf(outfile, "%" PRIu64 " %" PRIu64 " %" PRIu64 " %" PRIu64 "\n", apstradati1, neapstradati1, nolakoti1, nokrasoti1);
 
-	uint64_t ptr1, ptr2;
-	ptr1 = ptr2 = 0;
 
-	int64_t last_indx = 0;
-	while (1)
-	{
-		if (ptr1 == ptr2 && ptr1 == last_indx)
-		{
-			ptr1 += n;
-			if (ptr1 <= k)
-				nokrasoti++;
-		}
+	// this is brute force approach but it is too slow for big numbers
+	// uint64_t ptr1, ptr2;
+	// ptr1 = ptr2 = 0;
 
-		if (ptr1 < ptr2 && ptr1 <= k)
-		{
-			ptr1 += n;
-			if (ptr1 <= k)
-				nokrasoti++;
-		}
-		else if (ptr2 < ptr1 && ptr2 <= k)
-		{
-			ptr2 += m;
-			if (ptr2 <= k)
-				nolakoti++;
-		}
-		if ((ptr1 == ptr2) && (ptr1 < k) && (last_indx != ptr1))
-		{
-			last_indx = ptr1;
-			neapstradati++;
-			nokrasoti--;
-			nolakoti--;
-		}
-		if (ptr1 > k && ptr2 > k)
-			break;
-	}
+	// int64_t last_indx = 0;
+	// while (1)
+	// {
+	// 	if (ptr1 == ptr2 && ptr1 == last_indx)
+	// 	{
+	// 		ptr1 += n;
+	// 		if (ptr1 <= k)
+	// 			nokrasoti++;
+	// 	}
 
-	apstradati = k - nokrasoti - nolakoti - neapstradati;
+	// 	if (ptr1 < ptr2 && ptr1 <= k)
+	// 	{
+	// 		ptr1 += n;
+	// 		if (ptr1 <= k)
+	// 			nokrasoti++;
+	// 	}
+	// 	else if (ptr2 < ptr1 && ptr2 <= k)
+	// 	{
+	// 		ptr2 += m;
+	// 		if (ptr2 <= k)
+	// 			nolakoti++;
+	// 	}
+	// 	if ((ptr1 == ptr2) && (ptr1 < k) && (last_indx != ptr1))
+	// 	{
+	// 		last_indx = ptr1;
+	// 		neapstradati++;
+	// 		nokrasoti--;
+	// 		nolakoti--;
+	// 	}
+	// 	if (ptr1 > k && ptr2 > k)
+	// 		break;
+	// }
 
-	fprintf(outfile, "%" PRIu64 " %" PRIu64 " %" PRIu64 " %" PRIu64 "\n", apstradati, neapstradati, nolakoti, nokrasoti);
+	// apstradati = k - nokrasoti - nolakoti - neapstradati;
+
+	// fprintf(outfile, "%" PRIu64 " %" PRIu64 " %" PRIu64 " %" PRIu64 "\n", apstradati, neapstradati, nolakoti, nokrasoti);
 	fclose(outfile);
 
 	return 0;
